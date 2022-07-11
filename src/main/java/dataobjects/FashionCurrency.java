cdf0dfae1c9eeb7f8b75be47a0bfebd74bb6f1de
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-11 10:13:51 +0200

    add dataobject package

---
+++ src/main/java/dataobjects/FashionCurrency.java
@@ -0,0 +1,15 @@
+package dataobjects;
+
+public class FashionCurrency {
+  private final String Pounds;
+  private final String Shillings;
+  private final String Pence;
+
+  public FashionCurrency(String pounds, String shillings, String pence) {
+    Pounds = pounds;
+    Shillings = shillings;
+    Pence = pence;test
+  }
+
+
+}
