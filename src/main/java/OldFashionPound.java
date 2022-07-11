0778c049c18c5e44cb6d0622aac9548cd9c1edf3
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-11 12:56:06 +0200

    uncomplete work

--- src/main/java/OldFashionPound.java
+++ src/main/java/OldFashionPound.java
@@ -1,8 +1,11 @@
+import dataobjects.FashionCurrency;
 import services.ManagementInput;
 
 public class OldFashionPound {
 
   public static void main(String[] args) {
+    FashionCurrency money1 = new FashionCurrency();
+    FashionCurrency money2 = new FashionCurrency();
   /*  Expression must follow one of this rule:
     Xp Ys Zd [+|-] Ap Bs Cd
     Xp Ys Zd [*|/] K
@@ -11,7 +14,7 @@ public class OldFashionPound {
     String input = args[0];
 //    System.out.println(input);
     ManagementInput manageInput = new ManagementInput(input);
-    manageInput.extractNumbersFromInput();
+    money1 = manageInput.extractNumbersFromInput();
 
 
   }
--- src/main/java/services/OperationsService.java
+++ src/main/java/services/OperationsService.java
@@ -1,17 +1,42 @@
 package services;
 
 import dataobjects.FashionCurrency;
+import exceptions.OperatorNotFound;
+import utils.OperatorsEnum;
 
 public class OperationsService {
 
-  FashionCurrency firstValue = new FashionCurrency();
-  FashionCurrency secondValue = new FashionCurrency();
+  private FashionCurrency firstValue = new FashionCurrency();
+  private FashionCurrency secondValue = new FashionCurrency();
+  private int divOrMul;
+  private OperatorsEnum operator;
 
-  public OperationsService(FashionCurrency firstValue, FashionCurrency secondValue){
+  public OperationsService(FashionCurrency firstValue, FashionCurrency secondValue,
+      OperatorsEnum operator, int divOrMul) {
     this.firstValue = firstValue;
     this.secondValue = secondValue;
+    this.operator = operator;
+    this.divOrMul = divOrMul;
   }
 
+  public FashionCurrency runOperation() throws OperatorNotFound {
+    FashionCurrency money = new FashionCurrency();
+    switch (operator.toString()) {
+      case "SUM":
+        Add sum = new Add(firstValue,secondValue);
+        return sum.calc();
+      case "MINUS":
+        Minus minus = new Minus(firstValue,secondValue);
+        return minus.calc();
+      case "DIVISION":
+        Division division = new Division(firstValue,divOrMul);
+        return division.calc();
+      case "MULTIPLICATION":
+        Multiplication multply = new Multiplication(firstValue,divOrMul);
+        return multply.calc();
+    }
+    throw new OperatorNotFound("The mathematical operator could not be found");
+  }
 
 
 }
