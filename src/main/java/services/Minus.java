66686be106e8b5dde984db5e88fe2aed7eb220c9
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-11 11:04:01 +0200

    add minus class

---
+++ src/main/java/services/Minus.java
@@ -0,0 +1,37 @@
+package services;
+
+import dataobjects.FashionCurrency;
+
+public class Minus {
+
+  private FashionCurrency leftElement;
+  private FashionCurrency rightElement;
+
+  public Minus(FashionCurrency leftElement, FashionCurrency rightElement) {
+    this.leftElement = leftElement;
+    this.rightElement = rightElement;
+  }
+
+
+  public FashionCurrency Calc(FashionCurrency leftElement, FashionCurrency rightElement) {
+    int lPence = leftElement.getPence();
+    int lShillings = leftElement.getShillings();
+    int lPound = leftElement.getPounds();
+
+    int rPence = rightElement.getPence();
+    int rShillings = rightElement.getShillings();
+    int rPound = rightElement.getPounds();
+
+    // Convert all in pence
+    lPence = lPence + lShillings * 12 + lPound * 20 * 12;
+    rPence = rPence + rShillings * 12 + rPound * 20 * 12;
+
+    int pencesTotal = lPence - rPence;
+
+    OperationsService manageOp = new OperationsService();
+
+    return manageOp.extractValue(pencesTotal, 0, 0);
+
+  }
+
+}
