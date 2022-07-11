506fa6a6ee455bce920fe0f16235eed277e526b7
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-11 12:05:59 +0200

    move extravalues to FashionCurrency class

--- src/main/java/OldFashionPound.java
+++ src/main/java/OldFashionPound.java
@@ -1,8 +1,19 @@
+import services.ManagementInput;
 
 public class OldFashionPound {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+  /*  Expression must follow one of this rule:
+    Xp Ys Zd [+|-] Ap Bs Cd
+    Xp Ys Zd [*|/] K
+   */
+
+    String input = args[0];
+//    System.out.println(input);
+    ManagementInput manageInput = new ManagementInput(input);
+    manageInput.extractNumbers();
+
+
   }
 
 }
