0427f8eb07429141bb25a2787fe0f5d538c0ecc3
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-11 12:06:40 +0200

    add and correct extractValue method

--- src/main/java/dataobjects/FashionCurrency.java
+++ src/main/java/dataobjects/FashionCurrency.java
@@ -1,10 +1,12 @@
 package dataobjects;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class FashionCurrency {
   private int pounds;
   private int shillings;
@@ -14,6 +16,19 @@ public class FashionCurrency {
     this.pounds = 0;
     this.shillings = 0;
     this.pence = 0;
+  }
+
+  public void extractValue(int pence, int shillings, int pound) {
+    int penceValue = pence % 12;
+    int addShillings = pence / 12;
+    int shillingsValue = (shillings + addShillings) % 20;
+    int addPounds = (shillings + addShillings) / 20;
+    int poundsValue = pound + addPounds;
+
+    this.pence = penceValue;
+    this.shillings = shillingsValue;
+    this.pounds = poundsValue;
+
   }
 
 
